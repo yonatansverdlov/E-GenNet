be37796b9b3beb4e202fc83a2e6f54724103510f
yonatansverdlov <yonatansverdlov276@gmail.com>
2024-05-23 13:19:48 +0300

    Rearrange and make Abstarct_model

--- chemical_propery_exps.py
+++ chemical_propery_exps.py
@@ -10,7 +10,7 @@ types = 'BDE'
 # Choose B5 but could be also burL, burB5, L.
 task = 'BindingEnergy'
 
-test_acc, val_acc, train_acc = train_type_n_times(types=types, task=task, metric_track='acc', fix_seed=False,epochs=1500)
+test_acc, val_acc, train_acc = train_type_n_times(types=types, task=task, metric_track='acc', fix_seed=False,epochs=1200)
 
 print(f"Train acc {train_acc} in task {task}")
 
