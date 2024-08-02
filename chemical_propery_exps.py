51f7f685dc91e73d0d14d45b9d455c199ec6f5a7
STAFF\yonatans <yonatansverdlov276@gmail.com>
2024-08-02 12:16:34 +0300

    update

--- chemical_propery_exps.py
+++ chemical_propery_exps.py
@@ -3,18 +3,21 @@ The training of chemical properties.
 """
 import sys
 from exps.utils import train_type_n_times
-arg_parse = False
-if arg_parse:
-    types = str(sys.argv[1])
-    task = str(sys.argv[2])
-else:
-    # Can be Drugs, BDE, Kraken.
+from argparse import ArgumentParser
 
-    types = 'Kraken'
-    # Choose B5 but could be also burL, burB5, L.
-    task = 'B5'
+parser = ArgumentParser()
+parser.add_argument("--types", dest="task", default='Kraken', type=str, choices= ['Kraken','Drugs','BDE'],
+                    required=False)
+parser.add_argument("--task", dest="type", default='B5', type=str, choices=['B5','L','burB5','burL','ip','ea','chi'],
+                    required=False)
+parser.add_argument("--batch_size", dest="dim", default=20, type=int, required=False)
 
-test_acc, val_acc, train_acc = train_type_n_times(types=types, task=task, metric_track='acc', fix_seed=False,epochs=1500)
+args = parser.parse_args()
+
+accum_grad = 40// args.batch_size
+
+test_acc, val_acc, train_acc = train_type_n_times(types=args.types, task=args.task, metric_track='acc', fix_seed=False,epochs=1200,bs = args.batch_size,
+accum_grad = accum_grad)
 
 print(f"Train acc {train_acc} in task {task}")
 
